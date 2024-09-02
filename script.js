4a2aaf997e042ed572c192f35d3f71d0dce67942
idrakseyfullayev <idrak.seyfi87@gmail.com>
2024-09-02 17:09:52 +0400

    update last

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let github = document.getElementById('github')
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+github.addEventListener('click',()=>{
+  location.href ='./projects.html'
+})
+

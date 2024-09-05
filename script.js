d4d6f8345afbe3e5b749a052c71470b584f6d7d6
idrakseyfullayev <idrak.seyfi87@gmail.com>
2024-09-05 12:29:09 +0400

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let live_demo = document.getElementById('live demo')
+let ana_sehife = document.getElementById('ana sehife')
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
@@ -7,19 +8,24 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  var homeButton = document.getElementById("ana sehife");
-  if (homeButton) {
-      homeButton.addEventListener("click", function() {
-          window.location.href = "index.html"; // Ana səhifəyə keçid üçün uyğun linki dəyişdirin
-      });
-  }
-});
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   var homeButton = document.getElementById("ana sehife");
+//   if (homeButton) {
+//       homeButton.addEventListener("click", function() {
+//           window.location.href = "index.html"; // Ana səhifəyə keçid üçün uyğun linki dəyişdirin
+//       });
+//   }
+// });
+
+
 
 
 live_demo.addEventListener('click',()=>{
   location.href ='./projects.html'
 })
 
-
+ana_sehife.addEventListener('click',()=>{
+  location.href ='./index.html'
+})
 
